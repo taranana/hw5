67cf1cef2381aeed2560cd37762cda17d6300265
Taran Anand <taranana@usc.edu>
2025-12-06 01:34:23 +0000

    hw5 done

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool assignWorker(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<size_t>& shiftsUsed, size_t day, size_t slot);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -35,7 +35,16 @@ bool schedule(
     if(avail.size() == 0U){
         return false;
     }
+
+    size_t numDays = avail.size();
+    size_t numWorkers = avail[0].size();
     sched.clear();
+
+    sched.resize(numDays, vector<Worker_T>(dailyNeed,INVALID_ID));
+
+    vector<size_t> shiftsUsed(numWorkers,0);
+
+    return assignWorker(avail, dailyNeed, maxShifts, sched, shiftsUsed, 0 ,0);
     // Add your code below
 
 
@@ -43,3 +52,51 @@ bool schedule(
 
 }
 
+
+bool assignWorker(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<size_t>& shiftsUsed, size_t day, size_t slot) {
+    size_t numDays = avail.size();
+    size_t numWorkers = avail[0].size();
+
+    if(day == numDays) {
+        return true;
+    }
+
+
+    if(slot == dailyNeed) {
+        return assignWorker(avail,dailyNeed,maxShifts,sched,shiftsUsed,day+1,0);
+    }
+
+    for(size_t w = 0; w < numWorkers; w++){
+        if(!avail[day][w]) {
+            continue;
+        }
+
+        if(shiftsUsed[w] >= maxShifts) {
+            continue;
+        }
+
+        bool used = false;
+        for(size_t x = 0; x < slot; x++) {
+            if(sched[day][x] == w){
+                used = true;
+                break;
+            }
+        }
+        if(used) {
+            continue;
+        }
+
+        sched[day][slot] = w;
+        shiftsUsed[w]++;
+
+        if(assignWorker(avail,dailyNeed,maxShifts,sched,shiftsUsed,day,slot+1)) {
+            return true;
+        }
+
+        sched[day][slot] = INVALID_ID;
+        shiftsUsed[w]--;
+
+    }
+
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void makeWords(string& current, const string& in, const string& floating, size_t index,int dashesLeft, const set<string>&dict, set<string>& results);
 
 
 // Definition of primary wordle function
@@ -22,7 +23,79 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+  set<string> results;
+  string current = in;
+  int dashesLeft = 0;
+  for(char c : in) {
+    if(c == '-') {
+      dashesLeft++;
+    }
+  }
+  makeWords(current, in ,floating, 0,dashesLeft, dict, results);
+  return results;
 }
 
 // Define any helper functions here
+void makeWords(string& current, const string& in, const string& floating, size_t index,int dashesLeft, const set<string>& dict, set<string>& results) {
+  if(index == in.size()) {
+    if(floating.empty() && dict.find(current) != dict.end()) {
+      results.insert(current);
+    }
+    return;
+  }
+
+  if(in[index] != '-') {
+    current[index] = in[index];
+    makeWords(current,in,floating,index +1,dashesLeft, dict, results);
+    return;
+  }
+
+  int floatingSpace = floating.size(); 
+
+  if(dashesLeft == floatingSpace) {
+    for(size_t i = 0; i < floating.size(); i++) {
+      char c = floating[i];
+      char old = current[index];
+      current[index] = c;
+
+      string next = floating;
+      next.erase(i,1);
+
+      makeWords(current, in, next, index + 1,dashesLeft-1, dict, results);
+
+      current[index] = old;
+    
+    }
+    return;
+  }
+
+  // asoidjadsj idk how to make less than 6 sec
+  for(char c = 'a'; c <= 'z'; c++) {
+    char old = current[index];
+    current[index] = c;
+
+    string next = floating;
+
+    /*for(size_t i = 0; i< next.size(); i++) {
+      if(next[i] == c) {
+        next.erase(i,1);
+        break;
+      }
+    } */
+
+    if(next.find(c) < next.size()) {
+      next.erase(next.find(c),1);
+    }
+
+
+    if((int)next.size() > dashesLeft-1) {
+      current[index] = old;
+      continue;
+    }
+
+  
+    makeWords(current, in,next,index+1,dashesLeft-1, dict, results);
+
+    current[index] = old;
+  }
+}
